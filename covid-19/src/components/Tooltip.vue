aa483df49a49876a1ac53ac6d860bd5f3afaa305
tecknovice <tecknovice@gmail.com>
2020-05-22 16:00:08 +0700

    update covid-19

---
+++ covid-19/src/components/Tooltip.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="tooltip" ref="tooltip" :style="tooltipStyle" :class="{ hover: show }">
+    <div>{{this.statistics!=null?this.statistics.name:'No statistics'}}</div>
+    <div v-if="statistics">
+      Confirmed:
+      <span>{{statistics.confirmed}}</span>
+    </div>
+    <div v-if="statistics">
+      Deaths :
+      <span>{{statistics.deaths}}</span>
+    </div>
+    <div v-if="statistics">
+      Recovered:
+      <span>{{statistics.recovered}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tooltip",
+  props: ["position", "statistics", "show"],
+  data() {
+    return {
+      tooltipStyle: {
+        left: 0,
+        top: 0
+      }
+    };
+  },
+  watch: {
+    position: function(value) {
+      let tooltipWidth = this.$refs.tooltip.clientWidth;
+      let tooltipHeight = this.$refs.tooltip.clientHeight;
+      let [X, Y] = value;
+      this.tooltipStyle.left = X - tooltipWidth / 2 + "px";
+      this.tooltipStyle.top = Y - tooltipHeight - 25 + "px";
+      console.log(X, Y);
+      console.log(this.statistics);
+    }
+  }
+};
+</script>
+
+<style scoped>
+#tooltip {
+  text-align: left;
+  visibility: hidden;
+  background-color: #555;
+  color: #fff;
+  border-radius: 6px;
+  padding: 5px;
+  position: absolute;
+  z-index: 1;
+  opacity: 0;
+  transition: opacity 0.3s;
+}
+
+#tooltip::after {
+  content: "";
+  position: absolute;
+  top: 100%;
+  left: 50%;
+  margin-left: -10px;
+  border-width: 10px;
+  border-style: solid;
+  border-color: #555 transparent transparent transparent;
+}
+
+#tooltip.hover {
+  visibility: visible;
+  opacity: 0.7;
+}
+</style>
--- covid-19/src/components/WorldMap.vue
+++ covid-19/src/components/WorldMap.vue
@@ -1,73 +1,104 @@
 <template>
   <div id="content" :style="mapStyle" ref="mapContent">
+    <tooltip :position="position" :statistics="statistics" :show="show" />
     <svg width="100%" height="100%">
-      <g class="map"/>
+      <g class="map" />
     </svg>
   </div>
 </template>
 
 <script>
-import * as d3 from "d3";
+import * as d3 from "d3"
+import Tooltip from "./Tooltip"
+import getCountryISO2 from "country-iso-3-to-2"
 export default {
   name: "WorldMap",
+  components: {
+    Tooltip
+  },
   data() {
-    return{
+    return {
       projection: null,
       geoGenerator: null,
       geoJson: null,
-      mapStyle:{
-        height: 'auto'
-      }
+      mapStyle: {
+        height: "auto"
+      },
+      position: [],
+      statistics: null,
+      show: false
     }
   },
-  created(){
-    this.projection = d3.geoEquirectangular()
+  created() {
+    // this.projection = d3.geoEquirectangular() //ratio 2.585
+    this.projection = d3.geoMercator() // ratio 1.546
     this.geoGenerator = d3.geoPath().projection(this.projection)
-    window.addEventListener("resize", this.updateMap);
-  },
-  destroyed(){
-    window.removeEventListener("resize", this.updateMap);
   },
   async mounted() {
-    let response = await fetch('/geojson/countries.geo.json');
+    let response = await fetch("/geojson/countries.geo.json")
     this.geoJson = await response.json()
-    this.createMap(this.geoJson);
+    this.createMap(this.geoJson)
   },
   methods: {
-    handleMouseover(data,index,map) {
+    handleMouseover(data, index, map) {
       d3.select(map[index]).attr("class", "highlight")
+      this.show = true
     },
-    handleMouseOut(data,index,map){
+    handleMouseOut(data, index, map) {
       d3.select(map[index]).attr("class", "normal")
+      this.show = false
+    },
+    handleMousemove(data, index, map) {
+      this.position = d3.mouse(map[index])
+      let iso3Code = data.id
+      let iso2Code = getCountryISO2(iso3Code)
+      let country = this.$store.state.countries.find(
+        item => item.CountryCode == iso2Code
+      )
+      console.log(country)
+      if (country != undefined) {
+        this.statistics = {
+          name: data.properties.name,
+          confirmed: country.TotalConfirmed,
+          deaths: country.TotalDeaths,
+          recovered: country.TotalRecovered
+        }
+      }else{
+        this.statistics = null
+      }
     },
     createMap(geoJson) {
-      let mapWidth = this.$refs.mapContent.clientWidth;
-      this.mapStyle.height = mapWidth/2.585 + 'px'
-      this.projection.fitExtent([[0, 0], [mapWidth, mapWidth/2.585]], geoJson);
+      let mapWidth = this.$refs.mapContent.clientWidth
+      this.mapStyle.height = mapWidth / 1.546 + "px"
+      this.projection.fitExtent(
+        [
+          [0, 0],
+          [mapWidth, mapWidth / 1.546]
+        ],
+        geoJson
+      )
       let svg = d3
         .select("#content g.map")
         .selectAll("path")
-        .data(geoJson.features);
-
-      svg.enter()
+        .data(geoJson.features)
+      svg
+        .enter()
         .append("path")
         .attr("d", this.geoGenerator)
         .on("mouseover", this.handleMouseover)
-        .on("mouseout", this.handleMouseOut);
-    },
-    updateMap(event){
-      console.log('updateMap:event', event)
-      let mapWidth = this.$refs.mapContent.clientWidth;
-      this.mapStyle.height = mapWidth/2.585 + 'px'
-      // this.projection.fitExtent([[0, 0], [mapWidth, mapWidth/2.585]], geoJson);
+        .on("mouseout", this.handleMouseOut)
+        .on("mousemove", this.handleMousemove)
     }
   }
-};
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-svg{
-  overflow: visible !important;
+svg {
+  overflow: visible !important
+}
+#content {
+  position: relative;
 }
 </style>
